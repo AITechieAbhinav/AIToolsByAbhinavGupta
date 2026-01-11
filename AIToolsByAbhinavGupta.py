45620b70e4b038e1a7b516dd784ae623e5427346
AITechieAbhinav <abhinavurfriend@gmail.com>
2026-01-11 20:01:01 +0530

    Update AIToolsByAbhinavGupta.py

--- AIToolsByAbhinavGupta.py
+++ AIToolsByAbhinavGupta.py
@@ -10,9 +10,6 @@ from langchain_huggingface.chat_models import ChatHuggingFace
 from langchain_community.callbacks import get_openai_callback
 from PyPDF2 import PdfReader
 
-from gtts import gTTS
-from io import BytesIO
-
 # Use a pipeline as a high-level helper
 from transformers import pipeline
 
@@ -20,9 +17,9 @@ st.set_page_config(page_title="AI Tools by Abhinav Gupta", layout="centered")
 
 st.title("📝 AI Tools by Abhinav Gupta")
 
-tab_titles = ["Text Summarizer","PDF QnA", "Text to Speech"]
+tab_titles = ["Text Summarizer","Document Summarizer"]
 
-tab1, tab2, tab3 = st.tabs(tab_titles)
+tab1, tab2 = st.tabs(tab_titles)
 
 with tab1:
 
@@ -34,7 +31,7 @@ with tab1:
 		sm_txt = summarizer(text)
 		st.markdown(sm_txt)
 
-with tab2:
+With tab2:
 
     api_key = st.secrets['api_key']
     
@@ -63,11 +60,8 @@ with tab2:
     #Save chunks
     data = text_spliter.split_text(text)
     
-    #if len(data) == 0:
-		#st.warning("Please upload a PDF file to continue.")
-        #st.stop()
-		
-
+    if len(data) == 0:
+        st.stop()
     
     #Create embeedings
     
@@ -92,30 +86,3 @@ with tab2:
             print(cb)
     
         st.write(response)
-
-with tab3 :
-
-  st.title("Simple Text to Speech Converter")
-
-  text_input = st.text_area("Enter text to convert to speech", height=150)
-
-  st.sidebar.title("Upload your file")
-  uploaded_file = st.sidebar.file_uploader("Choose a .txt file", type="txt")
-
-  if uploaded_file is not None:
-     file_text = uploaded_file.read().decode("utf-8")
-     st.subheader("Text from Uploaded file")
-     st.text(file_text)
-     text_input += "\n\n" + file_text
-
-  language = st.selectbox("Select language", ["en", "fr", "ru", "hi", "es"])
-
-  if st.button("Generate my speech"):
-      if text_input:
-          tts = gTTS(text_input, lang=language)
-          audio_stream = BytesIO()
-          st.success("Speech is generated successfully!")
-          tts.write_to_fp(audio_stream)
-          st.audio(audio_stream)
-      else:
-          st.warning("Please enter some text or upload from device.")
