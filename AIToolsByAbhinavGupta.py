9dfed2cce4c283c026a93c0d9bab12319c473bfb
AITechieAbhinav <abhinavurfriend@gmail.com>
2026-04-08 21:13:14 +0530

    Update AIToolsByAbhinavGupta.py

--- AIToolsByAbhinavGupta.py
+++ AIToolsByAbhinavGupta.py
@@ -22,36 +22,39 @@ tab_titles = ["AI Chatbot","Text Summarizer", "Text to Speech", "PDF QnA"]
 tab1, tab2, tab3, tab4= st.tabs(tab_titles)
 
 # ---------------- TAB 1 ----------------
-
-##Initiate Chat History##
-if "chat_history" not in st.session_state:
-    st.session_state.chat_history = []
-
-## Show chat history ##
-
-for message in st.session_state.chat_history:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
-
-llm = ChatGroq(
-    model= "llama-3.3-70b-versatile",
-    temperature=0.0
-)
-
-user_prompt = st.chat_input("Ask ChatBot")
-
-if user_prompt:
-    st.chat_message("user").markdown(user_prompt)
-    st.session_state.chat_history.append({"role":"user", "content":user_prompt})
+with tab1:
     
-    response = llm.invoke(
-        input = [{"role":"system", "content":"You are a helpful assistant"}, *st.session_state.chat_history]
-    )
-    assistant_response = response.content
-    st.session_state.chat_history.append({"role" :"assistant", "content" :assistant_response})
+    groq_api_key = st.secrets["groq_api_key"]
 
-    with st.chat_message("assitant"):
-        st.markdown(assistant_response)
+    ##Initiate Chat History##
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = []
+    
+    ## Show chat history ##
+    
+    for message in st.session_state.chat_history:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+    
+    llm = ChatGroq(
+        model= "llama-3.3-70b-versatile",
+        temperature=0.0
+    )
+    
+    user_prompt = st.chat_input("Ask ChatBot")
+    
+    if user_prompt:
+        st.chat_message("user").markdown(user_prompt)
+        st.session_state.chat_history.append({"role":"user", "content":user_prompt})
+        
+        response = llm.invoke(
+            input = [{"role":"system", "content":"You are a helpful assistant"}, *st.session_state.chat_history]
+        )
+        assistant_response = response.content
+        st.session_state.chat_history.append({"role" :"assistant", "content" :assistant_response})
+    
+        with st.chat_message("assitant"):
+            st.markdown(assistant_response)
     
 # ---------------- TAB 2 ----------------
 with tab2:
