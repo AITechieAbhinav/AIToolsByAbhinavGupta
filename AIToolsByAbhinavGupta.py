5dbc34d47e582dd604f33a30c890cef24bb35d09
AITechieAbhinav <abhinavurfriend@gmail.com>
2026-04-08 22:21:42 +0530

    Update AIToolsByAbhinavGupta.py

--- AIToolsByAbhinavGupta.py
+++ AIToolsByAbhinavGupta.py
@@ -30,6 +30,11 @@ with tab1:
     if "chat_history" not in st.session_state:
         st.session_state.chat_history = []
 
+    ## Show chat history ##
+    for message in (st.session_state.chat_history):
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
     llm = ChatGroq(
         model= "llama-3.3-70b-versatile",
         temperature=0.0,
@@ -50,11 +55,7 @@ with tab1:
     
         with st.chat_message("assistant"):
             st.markdown(assistant_response)
-
-    ## Show chat history ##
-    for message in reversed(st.session_state.chat_history):
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])  
+          
 # ---------------- TAB 2 ----------------
 with tab2:
     
